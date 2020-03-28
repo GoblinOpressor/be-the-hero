1fd7da9bde192ac5a8eb90758e4903a7c8d7699b
Goblin <hiroshi.iomori.01@gmail.com>
2020-03-28 17:38:59 -0300

    Front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,35 +1,36 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index (request, response) {
-        const { page = 1} = request.query;
+    async index(request, response) {
+        const { page = 1 } = request.query;
+
         const [count] = await connection('incidents').count();
-        
+
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-            .limit(5) 
-            .offset(( page - 1) * 5)   
+            .limit(5)
+            .offset((page - 1) * 5)
             .select([
-                'incidents.*',                
-                'ongs.name',
-                'ongs.email',
-                'ongs.whatsapp',
-                'ongs.city',
-                'ongs.uf'
+             'incidents.*',
+             'ongs.name',
+             'ongs.email',
+             'ongs.whatsapp',
+             'ongs.city',
+             'ongs.uf'
             ]);
-           
-           // .select('*');
-        
-        response.header('X-Total-count', count ['count(*)'] );
-            
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(incidents);
     },
 
-    async create(request, response){
-        const { title, description, value } = request.body;
+
+    
+    async create(request, response)  {
+        console.log(request.body);
+        const{ title, description, value } = request.body;
         const ong_id = request.headers.authorization;
-        
+
         const [id] = await connection('incidents').insert({
             title,
             description,
@@ -40,22 +41,21 @@ module.exports = {
         return response.json({ id });
     },
 
-    async delete(request, response ){
-        const {id} = request.params;
+    async delete(request, response) {
+        const { id } = request.params;
         const ong_id = request.headers.authorization;
 
-        const incidents = await connection('incidents')
+        const incident = await connection('incidents')
             .where('id', id)
             .select('ong_id')
             .first();
-        if (incidents.ong_id != ong_id){
-            return response.status(401).json({ erro: 'Operation not permitted.' });
+
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not allowed.' });
         }
 
         await connection('incidents').where('id', id).delete();
 
         return response.status(204).send();
-
-
-    },
+    }
 };
